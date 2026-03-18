316967f9b8111db0327473fa0dc0d855f0a6297b
BAY <basali1988@gmail.com>
2026-03-18 22:02:56 +0100

    miryoku+3

--- config/custom_config.h
+++ config/custom_config.h
@@ -3,13 +3,21 @@
 
 /*
  * Use QWERTY Miryoku while keeping the stock Corne mapping and behavior model
- * intact. Only the base layer tap keys are adjusted for the Swedish letters.
+ * intact. Only the base layer mapping is extended to use the right outer
+ * column for the Swedish letters.
  */
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_TAP_QWERTY
 
-#define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp LBKT,          \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),  \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SQT),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+#define MIRYOKU_LAYERMAPPING_BASE MIRYOKU_LAYOUTMAPPING_CORNE_SE
+
+#define MIRYOKU_LAYOUTMAPPING_CORNE_SE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&none K00 K01 K02 K03 K04          K05 K06 K07 K08 K09 &kp LBKT \
+&none K10 K11 K12 K13 K14          K15 K16 K17 K18 K19 &kp SEMI \
+&none K20 K21 K22 K23 K24          K25 K26 K27 K28 K29 &kp SQT  \
+               K32 K33 K34         K35 K36 K37
